36524fb381ebeb3b3ea1cb31d24213b0f572feaa
Anna Kusmierek <aniak1100@gmail.com>
2019-11-27 20:31:49 +0000

    write a function to handle watch

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 const secondHand = document.querySelector(".second-hand");
 const minHand = document.querySelector(".min-hand");
 const hoursHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  //   console.log(seconds);
+  const minutes = now.getMinutes();
+  const minDegrees = (minutes / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minDegrees}deg)`;
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 12) * 360 + 90;
+  hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+setInterval(setDate, 1000);
